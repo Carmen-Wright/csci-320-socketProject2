95bd7bcf5e98877486bfb9ac576a7553f2889b5d
Carmen Wright <99839370+Carmen-Wright@users.noreply.github.com>
2023-05-06 23:01:32 -0400

    Update client.py

--- client.py
+++ client.py
@@ -18,28 +18,32 @@ def get_file_size(file_name: str) -> int:
 
 
 def send_file(filename: str, address: (str, int)):
-    # get the file size in bytes
-    # TODO: section 2 step 2
+    file_size = get_file_size(filename)
+    size = (file_size).to_bytes(8, byteorder='big')
+    name = filename.encode()
 
-    # convert file_size to an 8-byte byte string using big endian
-    # TODO: section 2 step 3
 
     # create a TCP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        # TODO: section 2 step 5
-        # send the file size in the first 8-bytes followed by the bytes
-        # for the file name to server at (IP, PORT)
-        # TODO: section 2 step 6
-        # TODO: section 2 step 7
-        # open the file to be transferred
+        client_socket.connect((IP, PORT))
+        message = size + name
+        client_socket.sendto(message, (IP, PORT))
+        reply = client_socket.recv(BUFFER_SIZE)
+        if reply != b'go ahead':
+            raise Exception('Bad server response - was not go ahead!')
+
         with open(file_name, 'rb') as file:
-            # read the file in chunks and send each chunk to the server
             is_done = False
             while not is_done:
-                # TODO: section 2 step 8a
-                # TODO: section 2 step 8b
+                chunk = file.read(BUFFER_SIZE)
+                if len(chunk) > 0:
+                    client_socket.send(chunk)
+                elif len(chunk) == 0:
+                    is_done = True
+
+
     except OSError as e:
         print(f'An error occurred while sending the file:\n\t{e}')
     finally:
@@ -55,6 +59,7 @@ if __name__ == "__main__":
     # if an IP address is provided on cmdline, then use it
     if len(sys.argv) == 3:
         IP = sys.argv[2]
+        print(IP)
 
     send_file(file_name, (IP, PORT))
 
